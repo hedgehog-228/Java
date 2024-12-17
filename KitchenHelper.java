873a551e13c25956323a2c4af1515763c26aa79f
thanasis28 <it2023030@hua.gr>
2024-12-17 18:52:29 +0200

    Add files via upload

--- KitchenHelper.java
+++ KitchenHelper.java
@@ -3,39 +3,42 @@ package com.mycompany.kitchenhelper;
 import java.io.IOException;
 
 public class KitchenHelper {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         if (args.length == 0) {
             throw new IllegalArgumentException("Please give at least one argument.");
         }
 
         if (!args[0].equals("-list")) {
-            try {
-                RecipeParser recipeParser = new RecipeParser();
-                Recipe recipe = recipeParser.parse(args[0]);
+            RecipeParser recipeParser = new RecipeParser();
+            Recipe recipe = recipeParser.parse(args[0]);
 
-                int people = 1;
+            int people = 1;
 
-                if (args.length > 2 && args[1].equals("-people")) {
-                    try {
-                        people = Integer.parseInt(args[2]);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Number of people should be an integer.");
-                        return;
-                    }
-
-                    DisplayRecipe displayRecipe = new DisplayRecipe(recipe, people);
-                    displayRecipe.printRecipe();
+            if (args.length > 2 && args[1].equals("-people")) {
+                try {
+                    people = Integer.parseInt(args[2]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Number of people should be an integer.");
+                    return;
                 }
-
-            } catch (IOException e) {
-
             }
+
+            DisplayRecipe displayRecipe = new DisplayRecipe(recipe, people);
+            displayRecipe.printRecipe();
 
         } else if (args[0].equals("-list")) {
 
-        }
+            ShoppingList shoppingList = new ShoppingList();
+            RecipeParser recipeParser = new RecipeParser();
 
+            for (int i=1; i<args.length; i++) {
+                Recipe recipe = recipeParser.parse(args[i]);
+                shoppingList.addRecipe(recipe);
+            }
+
+            shoppingList.printShoppingList();
+        }
 
     }
 }
